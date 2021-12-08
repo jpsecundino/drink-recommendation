5b274d88a3abb18a5c82abe60c9f8167e60afde4
jpsecundino <jp.secundino@usp.br>
2021-12-07 21:26:21 -0300

    created flask backend
    co-authored-by: Gabriel <gabrie.ssouza@gmail.com>

---
+++ cocktail_recommendation.py
@@ -0,0 +1,77 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://colab:colab123@cluster0.sfizu.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+collection = db['cocktails']
+documents = list(collection.find())
+
+keys_to_preserve = ['cocktailDbId', 'name', 'type', 'glass', 'IBA']
+
+
+## Filter relevant properties, which is the ingredients plus glass, type, and IBA category
+def get_relevant_keys(docs):
+    non_relevant_keys = ['_id', 'instructions', 'image', 'IBA', 'name', '']
+    keys = [list(document.keys()) for document in documents]
+    unique_keys = list(set([item for sublist in keys for item in sublist]))
+    for key in non_relevant_keys:
+        unique_keys.remove(key);
+
+    return unique_keys;
+
+
+## Transform property value into a normalized numeric value
+def get_property_value(document, property):
+    if property in keys_to_preserve:
+        return document[property]
+
+    return 1 if document[property] else 0
+
+
+def create_data_matrix(keys):
+    data_matrix = [[0 for _ in range(len(keys))] for __ in range(len(documents))]
+    for i in range(len(documents)):
+        for j in range(len(keys)):
+            key = keys[j]
+            if key in documents[i]:
+                value = get_property_value(documents[i], key)
+                data_matrix[i][j] = value
+    return data_matrix
+
+
+unique_keys = get_relevant_keys(documents)
+data = create_data_matrix(unique_keys)
+
+"""## Pré-processamento"""
+
+import pandas as pd
+import numpy as np
+
+categorical_columns = ['glass', 'type']
+
+database = pd.DataFrame(data, columns=unique_keys)
+database = pd.get_dummies(database, columns=categorical_columns)
+
+ids = database['cocktailDbId'].astype(int)
+database = database.drop(['cocktailDbId'], axis=1)
+
+"""## Recomendando drinks"""
+
+
+def recommend(drink_id):
+    filter = ids != drink_id
+    X, y = database[filter], ids[filter]
+
+    query = database[np.logical_not(filter)]
+
+    from sklearn.neighbors import KNeighborsClassifier
+
+    recommender = KNeighborsClassifier(n_neighbors=5)
+    recommender.fit(X, y)
+
+    recommendation_indexes = recommender.kneighbors(query)[:][1][0]
+
+    import json
+    recommendations = {"recommendations": list(ids.iloc[recommendation_indexes].astype(str))}
+
+    return json.dumps(recommendations)
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from cocktail_recommendation import recommend
+
+app = Flask(__name__)
+
+
+@app.route('/<id>')
+def recommend_cocktail(id):
+    return recommend(int(id))
+
+
+if __name__ == '__main__':
+    app.run()
