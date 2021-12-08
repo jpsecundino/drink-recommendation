53db4c0a61a33093699fe0adaefb74f1858db90a
jpsecundino <jp.secundino@usp.br>
2021-12-07 22:05:27 -0300

    more tests to make heroku work

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from cocktail_recommendation import recommend
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    print('hello world!')
+
 @app.route('/<id>')
 def recommend_cocktail(id):
     return recommend(int(id))
